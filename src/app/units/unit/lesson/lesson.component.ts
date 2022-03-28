8fcce042ff87c4bb6d6a5418549ca0936fd545c3
karaniecV <vkaraniec@gmail.com>
2022-03-28 11:17:23 +0300

    all changes

--- src/app/units/unit/lesson/lesson.component.ts
+++ src/app/units/unit/lesson/lesson.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap} from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DataService } from '../../data.service';
 
@@ -15,6 +15,7 @@ export class LessonComponent implements OnInit {
   name: string | undefined;
   madalTask: any = '';
   openMadal = false;
+  isArray = false
 
   constructor(
     private dataService: DataService,
@@ -25,13 +26,15 @@ export class LessonComponent implements OnInit {
       .subscribe((paramMap: ParamMap) => {
         const data = this.dataService.getTasksByLesson(paramMap.get('id'));
         this.tasks = data
+
       });
   }
-  onOpenMadal(task:any){
+  onOpenMadal(task: any) {
     this.madalTask = task;
+    this.isArray = Array.isArray(task.src)
     this.openMadal = true;
   }
-  onCloseMadal(){
+  onCloseMadal() {
     this.madalTask = '';
     this.openMadal = false;
   }
